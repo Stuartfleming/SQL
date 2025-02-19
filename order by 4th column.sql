b8998e0df21e049aa51a6c6270fc8caa705f22ed
Stuartfleming <119344054+Stuartfleming@users.noreply.github.com>
2025-02-18 19:21:09 -0600

    Add files via upload

---
+++ order by 4th column.sql
@@ -0,0 +1,12 @@
+select *
+from companies
+order by employees desc;
+
+/* Your task is to sort the information in the provided table 'companies' by number of employees (high to low). Returned table should be in the same format as provided:
+
+companies table schema
+
+id
+ceo
+motto
+employees
